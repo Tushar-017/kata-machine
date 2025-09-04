64753c35d9a18d2de676bbcd15141778b83db830
Tushar Rajput <tusharo17rajput@gmail.com>
2025-09-04 07:27:37 +0530

    refactor: clean up Queue and Stack implementations with consistent formatting and comments

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,38 +1,36 @@
-type Node<T> ={
-    value: T,
-    next?: Node<T>
-}
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
 
 export default class Queue<T> {
     public length: number;
     private head?: Node<T>;
     private tail?: Node<T>;
 
-    
-
     constructor() {
         this.head = this.tail = undefined; // initializing head and tail to undefined
         // this.head = this.tail = null; // alternatively, you can use null if you
         // prefer to use null instead of undefined
-        // this.length = 0; // initializing length to 0
         this.length = 0;
+        // this.length = 0; // initializing length to 0
     }
 
     enqueue(item: T): void {
-        const node = { value: item } as Node<T>
+        const node = { value: item } as Node<T>;
         this.length++;
-        if(!this.tail) { // if(!this.head){
-            
-                this.tail = this.head = node; // if the queue is empty, both head and tail point to the new node
-                return; // no need to link the new node to the tail, as it is the only node in the queue
+        if (!this.tail) {
+            // if(!this.head){
+
+            this.tail = this.head = node; // if the queue is empty, both head and tail point to the new node
+            return; // no need to link the new node to the tail, as it is the only node in the queue
         }
         this.tail.next = node; // linking the new node to the end of the queue
         this.tail = node; // updating the tail to the new node
         // no need to update the head, as it remains the same
-
-}
+    }
     deque(): T | undefined {
-        if(!this.head) {
+        if (!this.head) {
             return undefined;
         }
         this.length--;
@@ -41,12 +39,12 @@ export default class Queue<T> {
 
         // freeing the old head
         head.next = undefined; // removing the reference to the next node from the old head
-        if(this.length === 0) {
+        if (this.length === 0) {
             this.tail = undefined; // if the queue is empty, set tail to undefined
         }
         return head.value; // returning the value of the old head
-}
+    }
     peek(): T | undefined {
         return this.head?.value; // returning the value of the head without removing it
-}
+    }
 }
--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,44 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        // initializing head to undefined
+        this.head = undefined;
+        // initializing length to 0
+        this.length = 0;
     }
 
     push(item: T): void {
-
-}
+        const node = { value: item } as Node<T>;
+        this.length++;
+        if (!this.head) {
+            this.head = node; // if the stack is empty, head points to the new node
+            return; // no need to link the new node to the previous head, as it is the only node in the stack
+        }
+        node.prev = this.head; // linking the new node to the previous head
+        this.head = node; // updating the head to the new node
+    }
     pop(): T | undefined {
+        this.length = Math.max(0, this.length - 1);
 
-}
+        if (this.length === 0) {
+            const head = this.head;
+            this.head = undefined; // if the stack is empty, set head to undefined
+            return head?.value; // returning the value of the old head
+        }
+
+        const head = this.head as Node<T>; // storing the current head to return it later¯
+        this.head = head.prev;
+        head.prev = undefined; // removing the reference to the previous node from the old head
+        return head.value; // returning the value of the old head
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value; // returning the value of the head without removing it
+    }
 }
